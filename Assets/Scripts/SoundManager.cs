201bbc9aa2b8ecb7ea22bc2ad9fb4086583c52f1
hab927 <47962153+hab927@users.noreply.github.com>
2026-03-07 01:39:18 -0800

    added sounds to folder

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,5 +2,64 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public AudioClip computerGlitch;
+    public AudioClip coinInsert;            // krokulator
+    public AudioClip coinsFalling;          // moogleoftheages
+    public AudioClip upgradePurchased;      // vilkas-sound
+    public AudioClip vendingConfirm;        // Emma7073
+    public AudioClip whoosh;                // hitrison
+    public AudioClip computerStartup;       // juskiddink
+    public AudioClip winSound;              // EVRetro
+    public AudioClip loseSound;             // Fupicat
 
+    public AudioClip expiredSting;          // jerry.berumen
+    public AudioClip freshSting;            // jerry.berumen
+    public AudioClip crunch;                // qubodup
+    public AudioClip glug;                  // deleted_user_2104797
+
+    public AudioSource src;
+
+    public void PlayCoinsFalling() {
+        src.PlayOneShot(coinsFalling);
+    }
+
+    public void PlayUpgradePurchased() {
+        src.PlayOneShot(upgradePurchased);
+    }
+
+    public void PLayVendingConfirm() {
+        src.PlayOneShot(vendingConfirm);
+    }
+
+    public void PlayWhoosh() {
+        src.PlayOneShot(whoosh);
+    }
+
+    public void PlayComputerStartup() {
+        src.PlayOneShot(computerStartup);
+    }
+
+    public void PlayWinSound() {
+        src.PlayOneShot(winSound);
+    }
+
+    public void PlayLoseSound() {
+        src.PlayOneShot(loseSound);
+    }
+
+    public void PlayExpiredSting() {
+        src.PlayOneShot(expiredSting);
+    }
+
+    public void PlayFreshSting() {
+        src.PlayOneShot(freshSting);
+    }
+
+    public void PlayCrunch() {
+        src.PlayOneShot(crunch);
+    }
+
+    public void PlayGlug() {
+        src.PlayOneShot(glug);
+    }
 }
